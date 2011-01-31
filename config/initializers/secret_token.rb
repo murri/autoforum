7d55f460d40ca58bf21c4bc8ca18ca730e6c68e7
Tommi <tommi.muranen@agilopartners.fi>
2011-01-31 11:13:38 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autoforum::Application.config.secret_token = '65f9b0fe0da9aeff1746443baa6f13b64d686cdaf67236444ce74b087545f93d837aa82727553c1281cc321cec6a8766a9243251e38c6a852745621c2504e618'
